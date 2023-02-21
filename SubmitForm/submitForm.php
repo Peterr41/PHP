1c8d7b110166e6ecd133e2d31ee223a7fecd6f85
Peterr41 <dnkhanh2000@gmail.com>
2023-02-22 01:34:01 +0700

    day 1

--- SubmitForm/submitForm.php
+++ SubmitForm/submitForm.php
@@ -4,30 +4,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
     <title>Print Data from form.html</title>
 </head>
 <body>
-    <div >
-        <table>
+    
+    <div class="container">
+        <table class="table table-bordered table-striped table-responsive-stack"  id="tableOne">
     
             <center><h1>Car Booking Form</h1></center>
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Number of Passengers</th>
+                    <th scope="col">Contact Address</th>
+                    <th scope="col">City</th>
+                    <th scope="col">Region</th>
+                    <th scope="col">Postal/Zip code</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">Pick Up Date</th>
+                    <th scope="col">Pick Up Time</th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><?php echo $_POST["Last"].' '.$_POST["First"] ?> <br></td>
+                    <td><?php echo $_POST['Email'] ?></td>
+                    <td><?php echo $_POST["Phone"] ?> <br></td>
+                    <td><?php echo $_POST["numberofPassengers"] ?></td>
+                    <td><?php echo $_POST["strAddress"] ?></td>
+                    <td><?php echo $_POST["City"] ?></td>
+                    <td><?php echo $_POST["Region"] ?></td>
+                    <td><?php echo $_POST["Postal"] ?></td>
+                    <td><?php echo $_POST["Country"] ?></td>
+                    <td><?php echo $_POST["bdate"] ?></td>
+                    <td><?php echo $_POST["btime"] ?></td>
+                </tr>
+            </tbody>
             
-            Your Name:<?php echo $_POST["Last"].' '.$_POST["First"] ?> <br>
-            Your Email: <input type="text" class="form-control" name="name" value="<?php echo $_POST['Email'] ?>"disabled > <br>
-            Your Phone: <?php echo $_POST["Phone"] ?> <br>
-            Number of Passengers: <?php echo $_POST["numberofPassengers"] ?> <br>
-            Contact Address: <?php echo $_POST["strAddress"] ?> <br>
-            City: <?php echo $_POST["City"] ?> <br>
-            Region: <?php echo $_POST["Region"] ?> <br>
-            Postal/Zip code: <?php echo $_POST["Postal"] ?> <br>
-            Country: <?php 
-            $country = $_POST["Country"];
-            echo $country ?>
-
-            Chose Vehicle: <br>
-
-            Pick Up Date: <?php echo $_POST["bdate"] ?> <br>
-            Pick Up Time: <?php echo $_POST["btime"] ?> <br>
         </table>
     </div>
 </body>

b43a634a503c8e1b66f7145808c02df9e4dd3fa1
Peterr41 <dnkhanh2000@gmail.com>
2023-02-24 15:55:07 +0700

    day 3

---
+++ SubmitForm/buttonLogin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login with Google</title>
+  <style>
+    .button {
+      background-color: #4285F4;
+      border: none;
+      color: white;
+      padding: 10px 20px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      margin: 10px;
+      cursor: pointer;
+    }
+  </style>
+</head>
+<body>
+  <a href="login.php"><button class="button">Login with Google</button></a>
+</body>
+</html>
--- SubmitForm/login.php
+++ SubmitForm/login.php
@@ -1,32 +1,62 @@
 <?php
-require_once './google/vendor/autoload.php';
- 
-// init configuration 
-$clientID = '798578647082-9uf403ei9dcpdjjsrvv109t22bn5hlgn.apps.googleusercontent.com';
-$clientSecret = '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';
-$redirectUri = 'http://localhost:3000/SubmitForm/form.php';
+require 'connect.php';
+// Bước 1: Thiết lập thông tin xác thực Google
+$client_id = '798578647082-9uf403ei9dcpdjjsrvv109t22bn5hlgn.apps.googleusercontent.com';
+$client_secret = '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';
+$redirect_uri = 'http://localhost:3000/SubmitForm/form.php';
+
+// Bước 2: Chuyển hướng người dùng đến trang xác thực Google
+if(!isset($_GET['code'])) {
+  $auth_url = 'https://accounts.google.com/o/oauth2/auth?response_type=code&client_id='.$client_id.'&redirect_uri='.$redirect_uri.'&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
+  header('Location: '.$auth_url);
+  die();
+}
+
+// Bước 3: Lấy mã truy cập từ Google
+$code = $_GET['code'];
+$url = 'https://accounts.google.com/o/oauth2/token';
+$data = array(
+  'code' => $code,
+  'client_id' => $client_id,
+  'client_secret' => $client_secret,
+  'redirect_uri' => $redirect_uri,
+  'grant_type' => 'authorization_code'
+);
+$options = array(
+  'http' => array(
+    'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+    'method' => 'POST',
+    'content' => http_build_query($data)
+  )
+);
+$context = stream_context_create($options);
+$result = json_decode(file_get_contents($url, false, $context));
+
+// Bước 4: Lưu thông tin đăng nhập vào session và cơ sở dữ liệu
+if(isset($result->access_token)) {
+  // Lưu thông tin đăng nhập vào session
+  session_start();
+  $_SESSION['access_token'] = $result->access_token;
+
+  // Lấy thông tin người dùng từ Google
+  $url = 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' . $result->access_token;
+  $user_info = json_decode(file_get_contents($url));
+
+  // Lưu thông tin người dùng vào cơ sở dữ liệu
+  if (!$conn) {
+    die('Kết nối đến cơ sở dữ liệu thất bại: ' . mysqli_connect_error());
+  }
   
-// create Client Request to access Google API 
-$client = new Google_Client();
-$client->setClientId($clientID);
-$client->setClientSecret($clientSecret);
-$client->setRedirectUri($redirectUri);
-$client->addScope("email");
-$client->addScope("profile");
- 
-// authenticate code from Google OAuth Flow 
-if (isset($_GET['code'])) {
-  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-  $client->setAccessToken($token['access_token']);
+  // Thêm bản ghi mới vào bảng users
+  $query = "INSERT INTO users (google_id, name, email) VALUES ('".$user_info->id."', '".$user_info->name."', '".$user_info->email."')";
   
-  // get profile info 
-  $google_oauth = new Google_Service_Oauth2($client);
-  $google_account_info = $google_oauth->userinfo->get();
-  $email =  $google_account_info->email;
-  $name =  $google_account_info->name;
- 
-  // now you can use this profile info to create account in your website and make user logged in. 
-} else {
-  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+  if (mysqli_query($conn, $sql)) {
+    echo "Thêm bản ghi mới thành công";
+  } else {
+    echo "Lỗi khi thêm bản ghi mới: " . mysqli_error($conn);
+  }
+  
+  // Đóng kết nối
+  mysqli_close($conn);
 }
 ?>
